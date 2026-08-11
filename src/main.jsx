d00aa895bd809d9463a27af113ffcb59630fca7d
thanhluansaoviet-prog <thanhluansaoviet@gmail.com>
2026-08-12 03:30:51 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,9 @@
-react-dom/client
-./App.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
